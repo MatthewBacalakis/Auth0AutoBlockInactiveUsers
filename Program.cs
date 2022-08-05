3247fc5c77b2352b6311415570e6a3af8bdef95b
Matt Bacalakis <matt.bacalakis@gmail.com>
2022-08-05 14:08:22 -0400

    Updated A0 and .net libraries.  Use A0 Auth SDK for token fetch.

--- Program.cs
+++ Program.cs
@@ -1,23 +1,24 @@
 ﻿using System;
 using System.Threading.Tasks;
+using Auth0.AuthenticationApi;
+using Auth0.AuthenticationApi.Models;
 using Auth0.ManagementApi;
 using Auth0.ManagementApi.Models;
-using Auth0.Core.Collections;
+using Auth0.ManagementApi.Paging;
 using Auth0.Core.Exceptions;
-using Auth0.Core.Http;
-using RestSharp;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Configuration.FileExtensions;
-using Microsoft.Extensions.Configuration.Json;
 using System.IO;
-using System.Collections.Generic;
-using System.Net;
 
 
 namespace AutomatedBlockUsers
 {
     class Program
     {
+        enum SearchCriteria
+        {
+            Login,
+            UserCreation
+        }
 
         static IConfigurationRoot configuration;
         static ManagementApiClient MgmtClient;
@@ -26,12 +27,14 @@ namespace AutomatedBlockUsers
         {
             //setup pulling config from appsettings.json
             var builder = new ConfigurationBuilder()
-            .SetBasePath(Directory.GetCurrentDirectory())
-            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
             configuration = builder.Build();
 
+            var auth0AuthClient = new AuthenticationApiClient(GetConfigValue("A0Domain"));
+
             //get token to call mgmt api
-            var token = (await GetMgmtApiToken()).AccessToken;
+            var token = (await GetMgmtApiToken(auth0AuthClient)).AccessToken;
 
             MgmtClient = new ManagementApiClient(token, GetConfigValue("A0Domain"));
 
@@ -40,7 +43,7 @@ namespace AutomatedBlockUsers
             if (args.Length > 0 && args[0] == "-u")
             {
                 int count = 0;
-                var allUsers = await SearchUsers(string.Empty, false, ",blocked");
+                var allUsers = await SearchUsers(string.Empty, SearchCriteria.UserCreation, ",blocked");
                 foreach (var user in allUsers)
                 {
                     if (user.Blocked == true)
@@ -59,12 +62,12 @@ namespace AutomatedBlockUsers
             Console.WriteLine($"Blocking users whose last login was {BlockThreshold} days ago.  Last login occurred on or before: {DateTimeOffset.UtcNow.AddDays(double.Parse(BlockThreshold) * -1).ToString("yyyy-MM-dd")})");
 
             //block users who have a login date
-            int usersBlockedByLogin = await BlockUsers(false);
+            int usersBlockedByLogin = await BlockUsers(SearchCriteria.Login);
             Console.WriteLine($"Blocked {usersBlockedByLogin} users based on last_login.");
             Console.WriteLine();
 
             //block users who have never logged in based on create date.
-            int usersBlockedByCreate = await BlockUsers(true);
+            int usersBlockedByCreate = await BlockUsers(SearchCriteria.UserCreation);
             Console.WriteLine($"Blocked {usersBlockedByLogin} users based on last_login.");
             Console.WriteLine($"Blocked {usersBlockedByCreate} users based on created_at.");
             Console.WriteLine($"Blocked {usersBlockedByLogin + usersBlockedByCreate} users in total");
@@ -75,13 +78,13 @@ namespace AutomatedBlockUsers
         /// Identifies and blocks all users based either on last_login or create date if user has never logged in
         /// as indicated by bool param
         /// </summary>
-        static async Task<int> BlockUsers(bool forUsersWithNoLogin)
+        static async Task<int> BlockUsers(SearchCriteria searchCriteria)
         {
             //build search query to fetch first (up to) 100 applicable users
-            var searchQuery = BuildUserSearchQuery(forUsersWithNoLogin);
+            var searchQuery = BuildUserSearchQuery(searchCriteria);
 
             //fetch users that match initial query
-            var users = await SearchUsers(searchQuery, forUsersWithNoLogin);
+            var users = await SearchUsers(searchQuery, searchCriteria);
 
             int blockedUsers = 0;
             User lastUser = null;
@@ -106,13 +109,13 @@ namespace AutomatedBlockUsers
 
                 //format last user's last_login or created_at date for search
                 string refineDate;
-                var unformatedDate = forUsersWithNoLogin ? lastUser.CreatedAt : lastUser.LastLogin;
+                var unformatedDate = searchCriteria == SearchCriteria.UserCreation ? lastUser.CreatedAt : lastUser.LastLogin;
                 refineDate = ((DateTime)unformatedDate).ToString("yyyy'-'MM'-'dd'T'HH':'mm':'ss'.'fff'Z'");
 
                 //get next batch of users to block.
                 Console.WriteLine($"Refine search to look for users after {refineDate}");
-                searchQuery = BuildUserSearchQuery(forUsersWithNoLogin, refineDate);
-                users = await SearchUsers(searchQuery, forUsersWithNoLogin);
+                searchQuery = BuildUserSearchQuery(searchCriteria, refineDate);
+                users = await SearchUsers(searchQuery, searchCriteria);
                 if (users.Count == 1 && users[0].UserId == lastUser?.UserId)
                     //doing refined search by last user's lastLogin could return same user since search
                     //is eventually consistent.  If they are the only user returned we are done
@@ -124,51 +127,49 @@ namespace AutomatedBlockUsers
             return blockedUsers;
         }
 
-        /// <summary>
-        /// Fetches an access token for the Auth0 Management api
-        /// </summary>
-        static async Task<TokenResponse> GetMgmtApiToken()
+        // <summary>
+        // Fetches an access token for the Auth0 Management api
+        // </summary>
+        static Task<AccessTokenResponse> GetMgmtApiToken(AuthenticationApiClient authClient)
         {
-            var client = new RestClient($"https://{GetConfigValue("A0Domain")}");
-            var request = new RestRequest("/oauth/token", Method.POST);
-
-            request.AddHeader("content-type", "application/json");
-            request.AddParameter("application/json", $"{{\"client_id\":\"{GetConfigValue("ClientId")}\",\"client_secret\":\"{GetConfigValue("ClientSecret")}\",\"audience\":\"https://mattb.auth0.com/api/v2/\",\"grant_type\":\"client_credentials\"}}", ParameterType.RequestBody);
-
-            var tokenResponse = await client.ExecuteTaskAsync<TokenResponse>(request);
-            return tokenResponse.Data;
+            var request = new ClientCredentialsTokenRequest()
+            {
+                Audience = $"https://{GetConfigValue("A0Domain")}/api/v2/",
+                ClientId = GetConfigValue("ClientId"),
+                ClientSecret = GetConfigValue("ClientSecret")
+            };
+            return authClient.GetTokenAsync(request);
         }
 
         /// <summary>
         /// Builds the search string passed to the user search endpoints q param.
         /// </summary>
-        static string BuildUserSearchQuery(bool forUsersWithNoLogin, string refineDate = "*")
+        static string BuildUserSearchQuery(SearchCriteria searchCriteria, string refineDate = "*")
         {
             var BlockThreshold = GetConfigValue("BlockThreshold");
             var BlockDate = DateTimeOffset.UtcNow.AddDays(double.Parse(BlockThreshold) * -1).ToString("yyyy-MM-dd");
 
-            //Enchancement Option: The search could be narrowed to users in a specific connection by including:
-            // identities.connection:"connection_name"
-            if (forUsersWithNoLogin)
+
+            if (searchCriteria == SearchCriteria.UserCreation)
                 // get unblocked users who have not logged in and whose created_at date was before blockdate
-                return $"-last_login:[* TO *] AND created_at:[{refineDate} TO {BlockDate}] AND -blocked:true";
+                return $"identities.connection:{GetConfigValue("ConnectionName")} AND -last_login:[* TO *] AND created_at:[{refineDate} TO {BlockDate}] AND -blocked:true";
             else
             {
                 // get unblocked users whose last login was before blockdate
-                return $"last_login: [{refineDate} TO {BlockDate}] AND -blocked:true";
+                return $"identities.connection:{GetConfigValue("ConnectionName")} AND last_login: [{refineDate} TO {BlockDate}] AND -blocked:true";
             }
         }
 
         /// <summary>
         /// Searches the management api for users to block
         /// </summary>
-        static async Task<IPagedList<User>> SearchUsers(string searchQuery, bool forUsersWithNoLogin, string additionalFields = "")
+        static async Task<IPagedList<User>> SearchUsers(string searchQuery, SearchCriteria searchCriteria, string additionalFields = "")
         {
             var userRequest = new GetUsersRequest();
 
             userRequest.Fields = $"user_id,last_login,blocked,created_at{additionalFields}";
             userRequest.Query = searchQuery;
-            userRequest.Sort = forUsersWithNoLogin ? "created_at:1" : "last_login:1";
+            userRequest.Sort = searchCriteria == SearchCriteria.UserCreation ? "created_at:1" : "last_login:1";
 
             var pageInfo = new PaginationInfo(0, int.Parse(GetConfigValue("UserPageSize")), true);
 
@@ -179,13 +180,7 @@ namespace AutomatedBlockUsers
             }
             catch (ApiException e)
             {
-                if (e.StatusCode == HttpStatusCode.TooManyRequests)
-                {
-                    //update failed due to mgmt api rate limits.  Try one more time after delay.
-                    await PauseForRateLimitReset(MgmtClient.GetLastApiInfo());
-                    Console.WriteLine($"Fetching users. query: {searchQuery}. ");
-                    return await MgmtClient.Users.GetAllAsync(userRequest, pageInfo);
-                }
+                Console.WriteLine(e);
                 throw;
             }
         }
@@ -201,45 +196,13 @@ namespace AutomatedBlockUsers
             try
             {
                 await MgmtClient.Users.UpdateAsync(user.UserId, new UserUpdateRequest { Blocked = blocked });
-                await ApplyThrottle(MgmtClient.GetLastApiInfo());
+
             }
             catch (ApiException e)
             {
-                if (e.StatusCode == HttpStatusCode.TooManyRequests)
-                {
-                    //update failed due to mgmt api rate limits.  Try one more time after delay.
-                    await PauseForRateLimitReset(MgmtClient.GetLastApiInfo());
-                    await MgmtClient.Users.UpdateAsync(user.UserId, new UserUpdateRequest { Blocked = blocked });
-                }
+                Console.WriteLine(e);
+                throw;
             }
-        }
-
-        /// <summary>
-        /// Checks if the mgmt api rate limit remaining response is below a certain threshold and if so
-        /// delays until limit has refreshed.  Prevents the process from consuming all available mgmt api calls.
-        /// </summary>   
-        static async Task ApplyThrottle(ApiInfo info)
-        {
-            if (info.RateLimit.Remaining < int.Parse(GetConfigValue("RateLimitThrottle")))
-            {
-                await PauseForRateLimitReset(info);
-            }
-        }
-
-        /// <summary>
-        /// Pauses until the mgmt api rate limit reset time has passed.
-        /// </summary>  
-        static async Task PauseForRateLimitReset(ApiInfo info)
-        {
-            var remaining = info.RateLimit.Remaining;
-            var reset = info.RateLimit.Reset;
-            var limit = info.RateLimit.Limit;
-
-            Console.WriteLine($"Rate Limit ({limit}) hit at {DateTimeOffset.UtcNow}, {remaining} remaining. Pausing until {reset}");
-
-            var delay = (int)(reset - DateTimeOffset.UtcNow).TotalMilliseconds;
-            await Task.Delay(delay > 0 ? delay : 0);
-            Console.WriteLine($"Resuming mgmt api calls after delay of {delay}.");
         }
 
         /// <summary>
